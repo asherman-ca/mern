e3707cd841d03cf25962fee04790165327d3eded
Alex Sherman <alex.sherman@MacBook-Pro-68.local>
2018-11-27 16:15:39 -0800

    profile schema

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// load Profile model
+const Profile = require("../../models/Profile");
+// load User model
+const User = require("../../models/User");
 
 // @route   GET api/profile/test
 // @desc    Tests profile route
@@ -10,5 +17,27 @@ router.get("/test", (req, res) => {
     msg: "Profile Works"
   });
 });
+
+// @route   GET api/profile
+// @desc    get current user's profile
+// @access  private
+
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "Profile not found for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
