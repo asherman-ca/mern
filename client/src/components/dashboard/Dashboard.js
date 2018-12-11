b2ddc5bfa41ce3befea0261cc727f237f510e8cd
Alex Sherman <alex@Alexs-MacBook-Pro.local>
2018-12-10 21:56:15 -0800

    dashboard buildout v1

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getCurrentProfile } from '../../actions/profileActions';
@@ -18,17 +19,31 @@ class Dashboard extends Component {
     if (profile === null || loading) {
       dashboardContent = <Spinner />;
     } else {
-      dashboardContent = <h1>Hello</h1>;
+      // check if logged in user has profile data
+      if (Object.keys(profile).length > 0) {
+        // has a profile
+        dashboardContent = <h4>TODO: Display Profile</h4>;
+      } else {
+        // User is logged in but has no profile
+        dashboardContent = (
+          <div>
+            <p className="lead text-muted">
+              Welcome to the Sandbox {user.name}
+            </p>
+            <p className="text-muted">Setup your account by adding a profile</p>
+            <Link to="/create-profile" className="btn btn-lg btn-info">
+              Create Profile
+            </Link>
+          </div>
+        );
+      }
     }
 
     return (
       <div className="dashboard">
         <div className="container">
           <div className="row">
-            <div className="col-md-12">
-              <h1 className="display-4">Dashboard</h1>
-              {dashboardContent}
-            </div>
+            <div className="col-md-12">{dashboardContent}</div>
           </div>
         </div>
       </div>
